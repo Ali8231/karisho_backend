ee0e0f9ae3cded3da06df16ba00764f025f196ac
Ali Samadzadeh <samadzadeh110@gmail.com>
2023-07-04 10:18:33 +0330

    Set environment and requirements
    UPDATE setting.py for import sensitive information from .env file
    CREATE pipfile for setting requirements for development stage
    CREATE requirements.txt for setting requirements for production stage

--- karisho/settings.py
+++ karisho/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2!qfrpwf@7-6&zh3esjljh)hum3e@7-#(b-dwwa1r61h56tq01'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,9 +82,9 @@ WSGI_APPLICATION = 'karisho.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'karisho_db',
-        'USER': 'root',
-        'PASSWORD': 'Ali@1382',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
